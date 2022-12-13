1151a681cc027aeb0a0237e234091585346fbb06
LMSerhii <leonovserhii89@gmail.com>
2022-12-13 19:45:51 +0200

    get_tth

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import csv
 import json
-
+import pprint
 import requests
 import pandas as pd
+from openpyxl import load_workbook
 
 from cookies_and_headers import cookies, headers
 
@@ -59,6 +60,45 @@ def get_data(custom_status_id, month='October'):
             comments = ', '.join([label.get('name').replace(' ', '') for label in labels])
             added_items = order.get('added_items')
 
+            params = {
+                'order_id': f'{id}',
+                'delivery_option_id': '4898969',
+                'is_np_pochtomat': 'true',
+                'cart_total_price': '692.8',
+            }
+
+            response = requests.get(
+                'https://my.prom.ua/remote/delivery/nova_poshta/init_data_order',
+                params=params,
+                cookies=cookies,
+                headers=headers,
+            ).json()
+
+            price = response.get('data').get('intDocNumber')
+
+            if price != None:
+                print(f'{id}: {price}')
+
+            else:
+                params = {
+                    'order_id': f'{id}',
+                    'delivery_option_id': '10119216',
+                }
+
+                response = requests.get(
+                    'https://my.prom.ua/remote/delivery/ukrposhta/init_data_order',
+                    params=params,
+                    cookies=cookies,
+                    headers=headers,
+                ).json()
+
+                price = response.get('data').get('declarationId')
+
+                if price != None:
+                    print(f'{id}: {price}')
+                else:
+                    print(f'{id}: ')
+
             for item in added_items:
                 sku = item.get('sku')
                 quantity = item.get('quantity')
@@ -84,9 +124,50 @@ def get_data(custom_status_id, month='October'):
                 read_file.to_excel(f'order_list_{month}.xlsx', index=None, header=True)
 
 
+def collect_data():
+    """  """
+    params = {
+        'order_id': '212914787',
+        'delivery_option_id': '10119216',
+    }
+
+    response = requests.get(
+        'https://my.prom.ua/remote/delivery/ukrposhta/init_data_order',
+        params=params,
+        cookies=cookies,
+        headers=headers,
+    ).json()
+
+    ttn = response.get('data').get('declarationId')
+
+    try:
+        price = int(response.get('data').get('declaredCost'))
+    except Exception:
+        price = ''
+
+    try:
+        post_pay = int(response.get('data').get('postPayDeliveryPrice'))
+    except Exception:
+        post_pay = ''
+
+    try:
+        deliveryCost = int(response.get('data').get('deliveryCost'))
+    except Exception:
+        deliveryCost = ''
+
+
+
+
+    print(f'{ttn} - {price} - {post_pay} - {deliveryCost}')
+
+
+
+
 def main():
-    get_data(custom_status_id=127894)
+    # get_data(custom_status_id=127894)
+    collect_data()
 
 
 if __name__ == '__main__':
     main()
+
